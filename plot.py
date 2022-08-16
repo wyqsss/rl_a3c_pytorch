61bdc32f987277b33aa0af098be0cdfe00bd13da
wyqsss <wyqsss@outlook.com>
2022-08-16 19:39:09 +0800

    modif

--- plot.py
+++ plot.py
@@ -4,7 +4,8 @@ from scipy.ndimage import gaussian_filter1d
 log_file1 = "run_logs/new_no_advice.log"
 log_file2 = "run_logs/new_RCMP.log"
 
-log_file3 = "run_logs/rand_advice.log"
+log_file3 = "run_logs/new_random.log"
+log_file4 = "run_logs/new_importance.log"
 
 def plt_log(logfile):
     data = open(logfile, 'r')
@@ -18,7 +19,7 @@ def plt_log(logfile):
         reward.append(float(items[11].split(',')[0]))
         if float(items[8].split(',')[0]) > 3000000:
             break
-    plt.plot(epoch, gaussian_filter1d(reward, sigma=2))
+    plt.plot(epoch, gaussian_filter1d(reward, sigma=1))
     print(f"epoch : {epoch[-1]}, reward : {reward[-1]}")
 
 # def plt_log_acc(logfile):
@@ -38,9 +39,34 @@ def plt_log(logfile):
 #     plt.plot(epoch, gaussian_filter1d(reward, sigma=2))
 #     print(f"epoch : {epoch[-1]}, reward : {reward[-1]}")
 
-plt_log(log_file1)
-plt_log(log_file2)
+def plt_advice(data_file):
+    data = open(data_file, 'r')
+    advice = []
+    epoch = []
+    for line in data:
+        items = line.split(" ")
+        if len(items) < 8 or items[7] != 'epoch':
+            continue
+        epoch.append(int(float(items[8].split(',')[0])))
+        left_advice = int(float(items[-1].split(',')[0]))
+        if left_advice > 0:
+            advice.append(10000 - left_advice)
+        else:
+            advice.append(10000)
+        if float(items[8].split(',')[0]) > 3000000:
+            break
+    plt.plot(epoch, advice)
+
+
+# plt_log(log_file1)
+# plt_log(log_file2)
 # plt_log(log_file3)
+# plt_log(log_file4)
+plt_advice(log_file1)
+plt_advice(log_file2)
+plt_advice(log_file3)
+plt_advice(log_file4)
+
 plt.margins(x=0)
-plt.legend(["noadvice", "rcmp"])
-plt.savefig("result")
+plt.legend(["noadvice", "rcmp", "random", "importance"])
+plt.savefig("advice")
--- run.bash
+++ run.bash
@@ -1,5 +1,5 @@
 # RCMP
-# nohup python main.py --env Pong-v0 --workers 24 --gpu-ids 0 1 2 4 --n_heads 5 --sigma 0.08 --amsgrad True > run_logs/new_rcmp_sig.log & 
+# nohup python main.py --env Pong-v0 --workers 24 --gpu-ids 0 1 2 4 --n_heads 5 --sigma 0.03 --amsgrad True > run_logs/new_rcmp_sig.log & 
 # no advice 
 # nohup python main.py --env Pong-v0 --workers 24 --gpu-ids 0 1 --budget 0 --amsgrad True > run_logs/new_no_advice.log &
 # random
